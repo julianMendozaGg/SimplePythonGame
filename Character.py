00a5de2535b14f9f239db2cc73c22098426d00eb
Julian Esteban Mendoza Wilches <julianmendoza777@gmail.com>
2020-06-15 22:47:53 -0500

    se separan los movimientos para aplicar adapter

--- Character.py
+++ Character.py
@@ -16,60 +16,55 @@ class Character(Sprite):
     def defPositions(self, auxPosX, auxPosY):
         self.posX = auxPosX
         self.posY = auxPosY
+# Se establecen los sprites y la imagen [1][0] sera la primera en dibujarse, ya que 1 y 0 son los primeros
+# valores por defecto
 
     def set_sprites(self, images):
         self.graphics = images
         self.image = self.graphics[self.direction][self.cont]
         #self.rect = self.sprite.get_rect()
 
-    def update(self):
-        #self.image= self.graphics [1][0]
+    def moveRight(self):
         pressed = pygame.key.get_pressed()
+        if pressed[K_RIGHT]:
+            self.posX += self.speed
+            time.sleep(0.06)
+            self.direction = 1
 
+    def moveLeft(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[K_LEFT]:
+            self.posX -= self.speed
+            time.sleep(0.06)
+            self.direction = 0
+
+    def doPower(self):
+        pressed = pygame.key.get_pressed()
         if pressed[K_SPACE]:
             self.direction = 4
             time.sleep(0.09)
 
-        if self.movementLeft:
-            if pressed[K_LEFT]:
-                self.posX -= self.speed
-                time.sleep(0.06)
-                self.direction = 0
-        if self.movementRight:
-            if pressed[K_RIGHT]:
-                self.posX += self.speed
-                time.sleep(0.06)
-                self.direction = 1
-
-                """elif pressed[K_UP]:
-            self.direction = 2
-            for i in range(0,3):
-                self.posY -= self.speed
-                self.image = self.graphics[self.direction][i]
-                print("me voy de la casa si esto no funiona xdd :(")
-                time.sleep(0.3)
-        elif pressed[K_DOWN]:
-            self.posY += self.speed
-            self.direction = 3
-            time.sleep(0.08)"""
-
-        
+    # Esta clase es la que ayuda a cambiar los sprites de los peronajes segun el movimiento
+    def changeSprite(self):
+        pressed = pygame.key.get_pressed()
         if pressed[K_RIGHT] or pressed[K_LEFT] or pressed[K_SPACE]:
-            self.image = self.graphics[self.direction][self.cont]
-            self.cont += 1
-            self.cont %= len(self.graphics[self.direction])
-
-            if self.posX < 0:
-                self.movementLeft = False
-            else:
-                self.movementLeft = True
-            if self.posX > 600:
-                self.movementRight = False
-            else:
-                self.movementRight = True
+            try:
+                self.image = self.graphics[self.direction][self.cont]
+                self.cont += 1
+                self.cont %= len(self.graphics[self.direction])
+            except IndexError:
+                self.cont = 0
         else:
             self.cont = 0
             self.image = self.graphics[self.direction][0]
+
+    # Este metodo reune todos los movimientos, los cuales le dan info. al metodo changeSprite y asi se puedan actualizar
+    # los sprites segun el movimiento
+    def update(self):
+        self.doPower()
+        self.moveRight()
+        self.moveLeft()
+        self.changeSprite()
 
     def drawCharacter(self, screen):
         screen.blit(self.image, (self.posX, self.posY))
